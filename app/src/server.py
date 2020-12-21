d3aa87e2510560fbd4bbcabd8fdaf9ac7d4e5cf8
rupel <rupel190c@gmail.com>
2020-12-22 00:11:58 +0100

    updated chatbot and server file (and requirements)

--- app/src/server.py
+++ app/src/server.py
@@ -1,4 +1,4 @@
-
+ 
 #import chatbot
 from flask import Flask, render_template, request
 import chatbot
@@ -12,16 +12,24 @@ def hello():
     
 @server.route("/detectIntent", methods=['POST'])
 def chat():
-    answer = chatbot.response(request.get_data().decode('utf-8'))
+
+    # variables used in response should be held here but can't be quickly changed
+    sentence = request.get_data().decode('utf-8')
+    answer = chatbot.response()
+    
     #if answer is None:
-    #    return 'Did not get an answer. Great. Just great.'
+    #    return 'Did not get an answer.
     return answer
     
     #request.get_data()
     #return request.get_json
     
 
-# FFS: needs to be at the bottom
+# FFS: needs to be at the bottom of course
 if __name__ == '__main__':
     server.run(host='0.0.0.0')
+    
+    
+    
+    
     
